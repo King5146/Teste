726e9010dab45f346b40864f69baf17a408c3978
King5146 <86003510+King5146@users.noreply.github.com>
2021-06-16 10:21:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let show = false;
+
+let list = document.getElementById('list');
+
+
+function showHide(getElementById) {
+	console.log(getElementById)
+if (show) {show = false; list.style.display = 'none'; e.innerHTML = "show"}
+else {show = true; list.style.display = 'block' ; e.innerHTML = "hide"}	
+}
